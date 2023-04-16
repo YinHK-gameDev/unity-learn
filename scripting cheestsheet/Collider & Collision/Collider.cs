89f38d24f172176d476667d5ddddc2021cf98338
Ken <67538811+YinHk@users.noreply.github.com>
2023-04-16 05:11:13 -0400

    Update Collider.cs

--- scripting cheestsheet/Collider & Collision/Collider.cs
+++ scripting cheestsheet/Collider & Collision/Collider.cs
@@ -12,8 +12,19 @@ public bool enabled;
 
 
 
+// Method
 
+// bool True when the ray intersects the collider, otherwise false.
+public bool Raycast(Ray ray, out RaycastHit hitInfo, float maxDistance);
 
+// OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.
+Collider.OnCollisionEnter(Collision);
+
+// OnCollisionExit is called when this collider/rigidbody has stopped touching another rigidbody/collider.
+Collider.OnCollisionExit(Collision);
+
+// OnCollisionStay is called once per frame for every collider/rigidbody that is touching rigidbody/collider.
+Collider.OnCollisionStay(Collision);
 
 
 
