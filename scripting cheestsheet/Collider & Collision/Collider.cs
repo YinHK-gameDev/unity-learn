38414c3116d96895c8d53e6656808d855728fb42
Ken <67538811+YinHk@users.noreply.github.com>
2023-04-16 05:01:25 -0400

    Create Collider.cs

---
+++ scripting cheestsheet/Collider & Collision/Collider.cs
@@ -0,0 +1,21 @@
+// Collider  
+
+
+// property
+
+// Is the collider a trigger?
+public bool isTrigger;
+
+// Enabled Colliders will collide with other Colliders, disabled Colliders won't.
+public bool enabled;
+
+
+
+
+
+
+
+
+
+
+
