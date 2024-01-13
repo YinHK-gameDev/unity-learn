dfa2126f436c4661854ff1318c9af85e565a25c5
Ken <67538811+YinHk@users.noreply.github.com>
2024-01-13 17:11:15 -0500

    Create Ray.cs

---
+++ scripting cheestsheet/raycast/Ray.cs
@@ -0,0 +1,33 @@
+// Ray class
+
+
+
+//Constructor
+public Ray(Vector3 origin, Vector3 direction);
+
+//eg:
+sing UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        // Create a ray from the transform position along the transform's z-axis
+        Ray ray = new Ray(transform.position, transform.forward);
+    }
+}
+
+//Properties
+
+//The direction of the ray.
+//Direction is always a normalized vector. If you assign a vector of non unit length, it will be normalized.
+public Vector3 direction;
+
+//The origin point of the ray.
+public Vector3 origin;
+
+
+//Methods
+
+//Returns a point at distance units along the ray.
+public Vector3 GetPoint(float distance);
