c3b3e5cff782cf4a1ce95495aa08a36f5f4a7967
Ken <67538811+YinHk@users.noreply.github.com>
2024-03-07 20:46:52 -0500

    Update Save&LoadFile.cs

--- Save & Load System/Save&LoadFile.cs
+++ Save & Load System/Save&LoadFile.cs
@@ -36,3 +36,31 @@ void FileExample(byte[] serizliedData)
       // TODO: Handle exception
    }
 }
+
+
+
+// Using JSON format to read and Save file
+
+
+public static void Save(object gameState, string fileName = "gamesave.dat")
+{
+   var serializedData = JsonUtility.ToJson(gameState);
+   var filePath = Application.persistentDataPath + "/" + fileName;
+   File.WriteAllBytes(filePath, serizliedData);
+}
+
+public static T Load<T>(string fileName = "gamesave.dat")
+{
+   var filePath = Application.persistentDataPath + "/" + fileName;
+   var serizliedData = ([]byte)(null)
+   try
+   {
+      serizliedData = File.ReadAllBytes(filePath);
+   }
+   catch (System.IO.FileNotFoundException)
+   {
+      return null;
+   }
+   return JsonUtility.FromJson<T>(serializedData);
+}
+
